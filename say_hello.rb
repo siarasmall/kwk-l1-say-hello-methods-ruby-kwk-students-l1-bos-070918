00c69c2107d2d172e0874ff5446ae7a513d680a8
siarasmall <siarasmall19@gmail.com>
2018-07-10 15:38:17 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,7 +2,7 @@
 your_name= gets.chomp.capitalize
 my_name= gets.chomp.capitalize
 
-def say_hello(your_name=Ruby Programmer,my_name=Siara)
+def say_hello(your_name=Ruby Programmer,my_name)
   puts "Hello #{your_name}! I'm #{my_name}"
 end 
 say_hello(your_name,my_name)

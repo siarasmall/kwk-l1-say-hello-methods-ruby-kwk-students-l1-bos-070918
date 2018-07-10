54024c208ab39a31ea0194de8eebef3e20ed6f32
siarasmall <siarasmall19@gmail.com>
2018-07-10 14:58:38 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # definition for say_hello method
 your_name= gets.chomp.capitalize
-def say_hello(your_name)
-  puts "Hello #{your_name}!"
+def say_hello(your_name),(my_name)
+  puts "Hello #{your_name}! I'm #{my_name}"
 end 
-say_hello(your_name)
+say_hello(your_name),(my_name)
